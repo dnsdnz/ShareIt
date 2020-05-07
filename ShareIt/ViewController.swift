a6661f2c30a3126692356205002725821928fd41
dnsdnz <berfz23@gmail.com>
2020-05-07 20:07:52 +0300

    first commit

---
+++ ShareIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareIt
+//
+//  Created by Deniz MacBook Air on 7.05.2020.
+//  Copyright © 2020 Deniz Cakmak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
