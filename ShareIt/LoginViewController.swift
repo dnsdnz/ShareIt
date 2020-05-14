707db8e015d24880bb639b55bbf40a17c24092ae
dnsdnz <berfz23@gmail.com>
2020-05-14 15:33:46 +0300

    Firebase updated

--- ShareIt/LoginViewController.swift
+++ ShareIt/LoginViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
+   
+    @IBOutlet weak var mailText: UITextField!
+    @IBOutlet weak var passText: UITextField!
+    
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: mailText.text!, password: passText.text!) { (user, error) in
+           if error == nil{
+             self.performSegue(withIdentifier: "loginToHome", sender: self)
+                          }
+            else{
+             let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            
+              alertController.addAction(defaultAction)
+              self.present(alertController, animated: true, completion: nil)
+                 }
+        }
+    }
+    
+    
+    @IBAction func goRegister(_ sender: Any) {
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
 
